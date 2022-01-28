81d34450e60df09281c6de807c45c7a1b7f6fe8f
shpil97 <89089556+shpil97@users.noreply.github.com>
2022-01-28 19:25:36 +0300

    Update 4_5.py

--- 4_5.py
+++ 4_5.py
@@ -1,7 +1,6 @@
 from sys import argv
 from utils import currency_rates
 
-if __name__ == '__main__':
-    currency, *args = argv
-    for i in args:
-        print(currency_rates(i))
+word = argv[1]
+print(currency_rates(word))
+
